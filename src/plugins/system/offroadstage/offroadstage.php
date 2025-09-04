af00231821ed225c052b1485a07ade8b29a7ca4a
Bojan Pavlovic <bojan.creator@gmail.com>
2025-09-04 15:51:40 +0200

    chore: remove deprecated offroadstage plugin; QA unchanged

--- src/plugins/system/offroadstage/offroadstage.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-declare(strict_types=1);
-// This plugin (offroadstage) has been removed and is no longer maintained.
-// File intentionally left as a stub to indicate deprecation. Do not use.
-return;
