a0d667c4f7317fa1b2c2bd28318b14f6ed233dad
Bojan Pavlovic <bojan.creator@gmail.com>
2025-09-04 13:43:35 +0200

    chore(repo): untrack inspection/build artifacts under tools/__inspect* and tools/__build; keep locally via .gitignore

--- tools/__inspect_external_plugin/4sef/__unpacked/plg_system/media/vendor/weeblr/forsef/assets/dist/admin.base.css.php
+++
@@ -1,1 +0,0 @@
-<?php /* @license GNU General Public License version 3; see LICENSE.md */ defined('_JEXEC') || defined('WBLIB_VERSION') || die; return 'admin-de52dc8581.base.css';
--- tools/__inspect_external_plugin/4sef/__unpacked/plg_system/sql/base/uninstall.sql
+++
@@ -1,1 +0,0 @@
-# 4SEF - Database support - Dropping database tables is done by the uninstall script in a user-controlled manner
